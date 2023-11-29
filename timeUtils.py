c4e762d0e272c58fef20dfe5e02563b1a06b1ff9
wjosuep13 <201504326@ingenieria.usac.edu.gt>
2023-11-28 21:35:29 -0600

    removed timeutils

--- timeUtils.py
+++
@@ -1,5 +0,0 @@
-    # Convierte el tiempo float en minutos a mm:ss
-def convertir_minutosfloat(duracion):
-    minutos = int(duracion)
-    segundos = round((duracion - minutos) * 60)
-    return f"{minutos}:{segundos:02d}"
