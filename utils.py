18615a8e5f5a811f97dc8735dc5e00ba92761454
wjosuep13 <201504326@ingenieria.usac.edu.gt>
2023-11-28 22:00:26 -0600

    updated mintutosToFloat error validation

--- utils.py
+++ utils.py
@@ -8,20 +8,23 @@ def convertir_minutosfloat(duracion):
 
 
 def minutosToFloat(duracion):
-    duracion = duracion.replace(" ","")
-    if not re.match(r"^\d+(:\d{1,2})?$", duracion):
-        raise ValueError("El formato de duración no es válido.")
+    duracion = duracion.replace(" ", "")
 
-    # Separamos minutos y segundos si están disponibles
+    # Acepta formatos mm:ss y mm
+    if not re.match(r"^\d{1,2}(:\d{1,2})?$", duracion):
+        print_error("El formato de duración no es válido. Debe ser mm:ss o mm.")
+        return 0.0
+
     partes = duracion.split(":")
     minutos = int(partes[0])
     segundos = int(partes[1]) if len(partes) == 2 else 0
 
-    # Validamos que los segundos no sean mayores a 59 y que los minutos y segundos no sean negativos
+    # Valida los minutos y segundos
     if segundos > 59 or minutos < 0 or segundos < 0:
-        raise ValueError("Los valores de minutos o segundos no son válidos.")
+        print_error("El formato de duración no es válido. Debe ser mm:ss o mm.")
+        return 0.0
 
-    return minutos + segundos / 60
+    return minutos + segundos / 60.0
 
 def print_error(message):
     print("\n\033[91m\033[1mERROR:\033[0m", message, "\n")
